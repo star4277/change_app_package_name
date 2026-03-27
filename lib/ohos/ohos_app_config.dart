aa85f01c08660442094baec20fdc878ed1d86fbc
Chibeiyu <chibeiyu9907@gmail.com>
2026-03-27 13:02:53 +0800

    ohos package replace support

--- lib/ohos/ohos_app_config.dart
+++ lib/ohos/ohos_app_config.dart
@@ -4,59 +4,45 @@ OhosApp ohosAppFromJson(String str) => OhosApp.fromJson(json.decode(str));
 
 String ohosAppToJson(OhosApp data, {bool pretty = false}) {
   if (pretty) {
-    return const JsonEncoder.withIndent('\t').convert(data.toJson());
+    return const JsonEncoder.withIndent('  ').convert(data.toJson());
   }
   return json.encode(data.toJson());
 }
 
 class OhosApp {
   App app;
+  final Map<String, dynamic> extraRoot;
 
   OhosApp({
     required this.app,
+    required this.extraRoot,
   });
 
   factory OhosApp.fromJson(Map<String, dynamic> json) => OhosApp(
         app: App.fromJson(json["app"]),
+        extraRoot: Map<String, dynamic>.from(json)..remove('app'),
       );
 
   Map<String, dynamic> toJson() => {
         "app": app.toJson(),
-      };
+      }..addAll(extraRoot);
 }
 
 class App {
   String bundleName;
-  String vendor;
-  int versionCode;
-  String versionName;
-  String icon;
-  String label;
+  final Map<String, dynamic> extraRoot;
 
   App({
     required this.bundleName,
-    required this.vendor,
-    required this.versionCode,
-    required this.versionName,
-    required this.icon,
-    required this.label,
+    required this.extraRoot,
   });
 
   factory App.fromJson(Map<String, dynamic> json) => App(
         bundleName: json["bundleName"],
-        vendor: json["vendor"],
-        versionCode: json["versionCode"],
-        versionName: json["versionName"],
-        icon: json["icon"],
-        label: json["label"],
+        extraRoot: Map<String, dynamic>.from(json)..remove('bundleName'),
       );
 
   Map<String, dynamic> toJson() => {
         "bundleName": bundleName,
-        "vendor": vendor,
-        "versionCode": versionCode,
-        "versionName": versionName,
-        "icon": icon,
-        "label": label,
-      };
+      }..addAll(extraRoot);
 }
